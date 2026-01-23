688ecb57af8961725a6e4c1b6c938254e59212d5
Junior331 <jarilson.rk@gmail.com>
2026-01-23 17:47:30 -0300

    fix: adjust padding and margins in AuthLayout for improved responsiveness
    - Reduced padding and margin values in the AuthLayout component to enhance the layout's responsiveness across different screen sizes.

--- src/components/auth/AuthLayout.tsx
+++ src/components/auth/AuthLayout.tsx
@@ -20,7 +20,7 @@ const AuthLayout: React.FC<AuthLayoutProps> = ({ children, className, logo=<Auth
           className="min-h-screen w-full flex items-center justify-center p-4 bg-cover bg-center bg-no-repeat"
           style={{ backgroundImage: `url(${getImage('login_bg').src})` }}
         >
-          <div className={cn("w-full max-w-[435px] py-[48px] px-[45px] rounded-[8px] bg-[#262626cc] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] backdrop-blur-[30px] scale-90 relative z-10", className)}>
+          <div className={cn("w-full max-w-[435px] py-[35px] px-[30px] sm:py-[48px] sm:px-[45px] rounded-[8px] bg-[#262626cc] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] backdrop-blur-[30px] scale-90 relative z-10", className)}>
             {logo && (
               <div className="flex justify-center mb-[24px]">
                 {logo}
